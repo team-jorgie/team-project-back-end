8dff5ef81a5f67a3f631cc35ae80b00d508e7945
omniczech <mikepetschek@gmail.com>
2018-05-09 13:52:51 -0400

    Adds file upload properly
    File is now converted to a stream before being passed in the body. File
    name is taken from command line, split before the file type, 4 bytes of
    random hex are inserted at the end to ensure files won't be overwritten.
    Mime-types allows for ContentType to be set in the request meaning the
    file will no longer be downloaded by default and has proper encoding.
    
    Command to run the script remains the same:
      'node bin/s3upload.js <file-path>'
    
    Packages added:
      - mime-types

--- bin/s3upload.js
+++ bin/s3upload.js
@@ -5,19 +5,52 @@ require('dotenv').config()
 
 const AWS = require('aws-sdk')
 const fs = require('fs')
+const crypto = require('crypto')
 const s3 = new AWS.S3()
+const path = require('path')
+const mime = require('mime-types')
+
+const promiseRandomBytes = () => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(4, (err, buf) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(buf.toString('hex'))
+      }
+    })
+  })
+}
 
 const file = process.argv[2]
+const fileType = mime.lookup(file)
+const stream = fs.createReadStream(file)
+
+const filesplit = file.split('.')
+
+promiseRandomBytes()
+  .then((crypt) => {
+    const filewithcrypt = [filesplit[0], '-' + crypt, '.' + filesplit[1]].join('')
+    return filewithcrypt
+  })
+  .then((crypt) => {
+    const params = {
+      ACL: 'public-read',
+      Body: stream,
+      Bucket: 'file-bucket-team',
+      ContentType: fileType,
+      Key: crypt
+    }
+    s3.upload(params, function (err, data) {
+      console.log(err, data)
+    })
+  })
 
 console.log('s3 time!', file)
 
-const params = {
-  // ACL: 'public-read',
-  Body: 'Body',
-  Bucket: 'file-bucket-team',
-  Key: 'key'
-}
-
-s3.upload(params, function (err, data) {
-  console.log(err, data)
-})
+// const params = {
+//   // ACL: 'public-read',
+//   Body: 'Body',
+//   Bucket: 'file-bucket-team',
+//   Key: 'key'
+// }
