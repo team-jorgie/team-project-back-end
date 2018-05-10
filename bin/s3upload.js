b1f5fcfe30f17f8259d031ecf90f66570d7e6e4e
omniczech <mikepetschek@gmail.com>
2018-05-10 11:32:28 -0400

    CLI upload script now creates DB record
    File size is dynamically generated through fs, url is made from response
    data from s3 on successful upload function. Owner is currently hardcoded
    and will be pulled in from js.
    
    invoke with 'node bin/s3upload.js <filepath> <file-title>'

--- bin/s3upload.js
+++ bin/s3upload.js
@@ -9,6 +9,18 @@ const crypto = require('crypto')
 const s3 = new AWS.S3()
 const path = require('path')
 const mime = require('mime-types')
+const mongoose = require('mongoose')
+const FileUpload = require('../app/models/fileupload')
+// assign native promises
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/filebucket-api-development', {
+  useMongoClient: true
+})
+
+const done = () => {
+  mongoose.connection.close()
+}
 
 const promiseRandomBytes = () => {
   return new Promise((resolve, reject) => {
@@ -22,30 +34,65 @@ const promiseRandomBytes = () => {
   })
 }
 
+const promiseS3 = (params) => {
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      console.log(data)
+      resolve(data)
+    })
+  })
+}
+
 const file = process.argv[2]
+const title = process.argv[3]
 const filename = path.basename(file)
+const fileStats = fs.statSync(file)
+const fileSizeInMegabytes = fileStats.size / 1000000.0
 const fileType = mime.lookup(file)
 const stream = fs.createReadStream(file)
 
 const filesplit = filename.split('.')
 
-promiseRandomBytes()
-  .then((crypt) => {
-    const filewithcrypt = [filesplit[0], '-' + crypt, '.' + filesplit[1]].join('')
-    return filewithcrypt
-  })
-  .then((crypt) => {
-    const params = {
-      ACL: 'public-read',
-      Body: stream,
-      Bucket: 'file-bucket-team',
-      ContentType: fileType,
-      Key: crypt
-    }
-    s3.upload(params, function (err, data) {
-      console.log(err, data)
+mongoose.connection.once('open', () => {
+  promiseRandomBytes()
+    .then((crypt) => {
+      const filewithcrypt = [filesplit[0], '-' + crypt, '.' + filesplit[1]].join('')
+      return filewithcrypt
     })
-  })
+    .then((crypt) => {
+      const params = {
+        ACL: 'public-read',
+        Body: stream,
+        Bucket: 'file-bucket-team',
+        ContentType: fileType,
+        Key: crypt
+      }
+      return params
+      // s3.upload(params, function (err, data) {
+      //   console.log(err, data)
+      //   let s3Response = data
+      //   return (s3Response)
+      // })
+    })
+    .then(promiseS3)
+    .then((s3Response) => {
+      // console.log(s3Response)
+      return FileUpload.create({
+        owner: '5af45560a3fcd30e62ea0ca0', // user ID
+        title: title, // From input
+        url: s3Response.Location, // working
+        size: fileSizeInMegabytes
+      })
+    })
+    .then((createdata) => {
+      console.log(createdata)
+    })
+    .catch(console.error)
+    .then(done)
+})
 
 console.log('s3 time!', file)
 
